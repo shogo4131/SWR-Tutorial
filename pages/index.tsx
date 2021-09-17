08e90763bb5e61646db4743dd0acf6d13132dafd
shogo4131 <shougo80697118@gmail.com>
2021-09-17 17:32:24 +0900

    immutable setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,18 @@
-import React, { ReactNode } from 'react';
-import type { NextPage, GetStaticProps } from 'next';
-import useSWR, { SWRConfig } from 'swr';
+import React from 'react';
+import type { NextPage } from 'next';
+import useSWR from 'swr';
+import useSWRImmutable from 'swr/immutable';
 
-const API = 'https://jsonplaceholder.typicode.com/posts';
-
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch(API);
-  const data = await res.json();
-
-  return {
-    props: {
-      fallback: {
-        [API]: data,
-      },
-    },
-  };
-};
-
-const Home: NextPage = ({ fallback }) => {
-  return (
-    <SWRConfig value={{ fallback }}>
-      <ChildComponent />
-    </SWRConfig>
+const Home: NextPage = () => {
+  /* immutableを使用することでリクエストが初回だけに設定できる */
+  const { data, error } = useSWRImmutable(
+    'https://jsonplaceholder.typicode.com/posts'
   );
-};
-
-const ChildComponent = () => {
-  const { data, error } = useSWR(API);
-
-  
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
-  return <div>{data.length}</div>;
+  return <>index</>;
 };
 
 export default Home;
